39108cd88212233ca89acfe0d09a56fb9434b2db
Yuxiang <liuyuxiang096@gmail.com>
2026-01-10 23:40:00 +0800

    Add Yahoo Finance news integration for news analyst
    - Added get_yfinance_news() function to fetch news from Yahoo Finance
    - Integrated yfinance as news_data vendor in interface.py
    - Updated default_config.py to include yfinance as news option
    - News function filters articles by date range and formats them properly
    - Supports date filtering using providerPublishTime from yfinance API

--- tradingagents/dataflows/interface.py
+++ tradingagents/dataflows/interface.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 # Import from vendor-specific modules
 from .local import get_YFin_data, get_finnhub_news, get_finnhub_company_insider_sentiment, get_finnhub_company_insider_transactions, get_simfin_balance_sheet, get_simfin_cashflow, get_simfin_income_statements, get_reddit_global_news, get_reddit_company_news
-from .y_finance import get_YFin_data_online, get_stock_stats_indicators_window, get_balance_sheet as get_yfinance_balance_sheet, get_cashflow as get_yfinance_cashflow, get_income_statement as get_yfinance_income_statement, get_insider_transactions as get_yfinance_insider_transactions
+from .y_finance import get_YFin_data_online, get_stock_stats_indicators_window, get_balance_sheet as get_yfinance_balance_sheet, get_cashflow as get_yfinance_cashflow, get_income_statement as get_yfinance_income_statement, get_insider_transactions as get_yfinance_insider_transactions, get_yfinance_news
 from .google import get_google_news
 from .openai import get_stock_news_openai, get_global_news_openai, get_fundamentals_openai
 from .alpha_vantage import (
@@ -98,6 +98,7 @@ VENDOR_METHODS = {
     # news_data
     "get_news": {
         "alpha_vantage": get_alpha_vantage_news,
+        "yfinance": get_yfinance_news,
         "openai": get_stock_news_openai,
         "google": get_google_news,
         "local": [get_finnhub_news, get_reddit_company_news, get_google_news],
